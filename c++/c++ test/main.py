e4c856c2a029a4a270d1b8122ab688245fa63a25
gabriel <gabriel@linux-romania.com>
2018-12-02 18:43:15 +0000

    2018_12_02

--- c++/c++ test/main.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-import ctypes
-lib = ctypes.CDLL('./test.so');
-lib.start()
-
-while True:
-	x=lib.get_value()
-	sleep(0.03)
-	print x
--- test_folder/c++/1.py
+++
@@ -1,6 +0,0 @@
-from time import sleep
-import ctypes
-lib = ctypes.CDLL('./test.so');
-x=lib
-print x
-
