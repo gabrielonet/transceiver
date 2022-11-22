c8e3656e71bcab0dc364ce523c0d6f6e0c096a48
gabrielonet <gabriel@linux-romania.com>
2022-11-22 18:59:29 +0200

    Update si5351_clock_builder_python.py

--- si5351_clock_builder_python.py
+++ si5351_clock_builder_python.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import math
 import re
 from math import floor
@@ -5,18 +6,47 @@ from fractions import Fraction
 import time
 from time import *
 
-freq = 7
+freq = 7.000123 ### in Mhz
+### VCO in range of 600 - 900 Mhz 
+multisynth_divider = 100 # define static multisynth divider for Freq out in range of 6 to 9 Mhz since VCO freq = Freq out x Multisynth 
+VCO = freq * multisynth_divider
+####  So, in case of 7 Mhz output, we need a 700Mhz VCO and a multisynth divider = 100
+#### Now we need to set VCO freq
 
 
-VCO = 900 # arbitrarry set at 900 Mhz
-ratio = VCO/freq
-M = floor(ratio)
-diff = ratio - M
-euclid = str(Fraction(diff))
-x = re.sub(r'/.*', '', str(euclid))
-y = re.sub(r'^.*?/', '', str(euclid))
+crystal = 25 ### Some models have 27 Mhz crystals inside, mine has 25 Mhz
 
-print (freq)
-print (M)
-print(x)
-print(y)
+##   VCO = 25 x (a+b/c)  , where 25 is Crystal oscillator and we need to get a, b and c values
+## As an example , if VCO = 700 then a+b/c = 700/25 =28
+## In this ideal case, we can set a=28 . then b/c = 0 as b=0 and c =1
+fractional_value = VCO / crystal
+
+print("Freq = " + str(freq) + "Mhz")
+print("VCO = " + str(VCO)+"Mhz")
+print("Multisynth divider = " + str(multisynth_divider))
+print("fractional value = " + str(fractional_value))
+
+###  If fractional_value is not idealy integer, then we should check this, make a = lowest integer part and compute difference
+
+
+if (fractional_value.is_integer()) :
+    print ("Fractional value is integer")
+    a = int(fractional_value)
+    b = 0
+    c = 1
+    diff = 0
+
+####   So far we got VCO , multisynth_divider, and fractional_value. From fractional value we got a, b and c values if fractional value is integer (ideal)
+###    Now it is time to get b and c values knowing te result of b/c. How that since multiple values are possible ? Using reverse Euclid, we need to determine first a and b set , not all !!
+###    We got to import Fraction from fractions, and that library will give us those 2 values    
+
+else:
+    print ("Fractional value nu este integer")
+    a = floor(fractional_value)
+    diff = fractional_value - a     
+    euclid = str(Fraction(diff))
+    a = re.sub(r'/.*', '', str(euclid))
+    b = re.sub(r'^.*?/', '', str(euclid))
+
+print ("a = "+ str(a))
+print ("b= "+ str(b))
